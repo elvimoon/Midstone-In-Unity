76042beeca3861535961ca92007b84b244218711
tienosca <tienoscar@hotmail.com>
2020-10-15 16:41:10 -0400

    Added restart if player dies

--- MidstoneUnityPort/Assets/Scripts/Player.cs
+++ MidstoneUnityPort/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //required for restarting game if player dies, needs access to the scene
 
 public class Player : MonoBehaviour
 {
@@ -16,6 +17,14 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        //Restart game if player dies by reloading the active scene
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //SceneManager.LoadScene("GameOver"); to load gameover scene instead
+        }
+
+
         //to enable player movement up/down to look better rather than instant teleportation
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
