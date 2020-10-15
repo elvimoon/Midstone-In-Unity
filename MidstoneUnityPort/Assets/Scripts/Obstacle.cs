e0ce33bfc215c3836b75c6732212ac3d05c10c86
tienosca <tienoscar@hotmail.com>
2020-10-15 14:27:22 -0400

    Added Enemy

---
+++ MidstoneUnityPort/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int damage = 1;
+    public float speed;
+
+
+    private void Update()
+    {
+        //Obstacle moving continually left
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Player taking damage upon collision with obstacle
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+    }
+}
--- MidstoneUnityPort/Assets/Scripts/Player.cs
+++ MidstoneUnityPort/Assets/Scripts/Player.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Vector2 targetPos;
-    //set public so we can change Y values of player movement in inspector window
     public float Yincrement;
+    //values set public to allow changing values in inspector window
+ 
     public float speed;
+    public float MaxHeight;
+    public float MinHeight;
+
+    public int health = 3;
 
     private void Update()
     {
@@ -15,12 +20,12 @@ public class Player : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
         //Player Movement Inputs Using Up/Down Arrow or W/S Key
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) && transform.position.y < MaxHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
             //transform.position = targetPos;
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) && transform.position.y > MinHeight)
         {
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
             //transform.position = targetPos;
